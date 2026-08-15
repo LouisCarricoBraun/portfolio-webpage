e0fc7d918b16399a80852e0c2161b613bfa2bfe0
Louis Carrico-Braun <louis.carricobraun@gmail.com>
2026-08-15 13:53:35 -0400

    Update profile.ts github profile link

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -2,5 +2,5 @@ export const profile = {
   name: "Louis Carrico-Braun",
   role: "Data Analyst & BI Developer",
   linkedin: "https://www.linkedin.com/in/louis-c-55361812b/",
-  github: "https://github.com/HughHashes",
+  github: "https://github.com/LouisCarricoBraun",
 };
